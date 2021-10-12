1879a108d83613ce1ed509c4d5dae99dd0c28bba
Kuba Jaruga <kubajaru@gmail.com>
2021-10-12 20:23:22 +0200

    Add moving empty field

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,72 @@
+import java.util.List;
+
 public class Board {
+
+    private final List<List<Integer>> board;
+    private int zeroFieldR;
+    private int zeroFieldC;
+    private String steps = "";
+
+
+    public Board(List<List<Integer>> board) {
+        this.board = board;
+        getZeroCords();
+    }
+
+    private void getZeroCords() {
+        for (int r = 0; r < this.board.size(); r++) {
+            for (int c = 0; c < this.board.get(r).size(); c++) {
+                if (this.board.get(r).get(c) == 0) {
+                    this.zeroFieldR = r;
+                    this.zeroFieldC = c;
+                }
+            }
+        }
+    }
+    
+    public List<List<Integer>> getBoard() {
+        return this.board;
+    }
+
+    private Integer getValue(int r, int c) {
+        return this.board.get(r).get(c);
+    }
+
+    private void setValue(int r, int c, int value) {
+        this.board.get(r).set(c, value);
+    }
+
+    public void moveUp() {
+        int upValue = getValue(this.zeroFieldR - 1, this.zeroFieldC);
+        setValue(this.zeroFieldR - 1, this.zeroFieldC, 0);
+        setValue(this.zeroFieldR, this.zeroFieldC, upValue);
+        this.zeroFieldR = this.zeroFieldR - 1;
+        this.steps = this.steps.concat("U ");
+    }
+
+    public void moveDown() {
+        int downValue = getValue(this.zeroFieldR + 1, this.zeroFieldC);
+        setValue(this.zeroFieldR + 1, this.zeroFieldC, 0);
+        setValue(this.zeroFieldR, this.zeroFieldC, downValue);
+        this.zeroFieldR = this.zeroFieldR + 1;
+        this.steps = this.steps.concat("D ");
+    }
+
+    public void moveLeft() {
+        int leftValue = getValue(this.zeroFieldR, this.zeroFieldC - 1);
+        setValue(this.zeroFieldR, this.zeroFieldC - 1, 0);
+        setValue(this.zeroFieldR, this.zeroFieldC, leftValue);
+        this.zeroFieldC = this.zeroFieldC - 1;
+        this.steps = this.steps.concat("L ");
+    }
+
+    public void moveRight() {
+        int rightValue = getValue(this.zeroFieldR, this.zeroFieldC + 1);
+        setValue(this.zeroFieldR, this.zeroFieldC + 1, 0);
+        setValue(this.zeroFieldR, this.zeroFieldC, rightValue);
+        this.zeroFieldC = this.zeroFieldC + 1;
+        this.steps = this.steps.concat("R ");
+    }
 }
+
+
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BoardTest {
+
+    @Test
+    void testMoveUp() {
+        List<List<Integer>> temp = new ArrayList<>();
+
+        temp.add(new ArrayList<>());
+        temp.add(new ArrayList<>());
+
+        temp.get(0).add(1); temp.get(0).add(2);
+        temp.get(1).add(0); temp.get(1).add(3);
+
+        Board board = new Board(temp);
+        board.moveUp();
+
+        Assertions.assertEquals(board.getBoard().get(0).get(0), 0);
+        Assertions.assertEquals(board.getBoard().get(1).get(0), 1);
+    }
+
+    @Test
+    void testMoveDown() {
+        List<List<Integer>> temp = new ArrayList<>();
+
+        temp.add(new ArrayList<>());
+        temp.add(new ArrayList<>());
+
+        temp.get(0).add(0); temp.get(0).add(2);
+        temp.get(1).add(1); temp.get(1).add(3);
+
+        Board board = new Board(temp);
+        board.moveDown();
+
+        Assertions.assertEquals(board.getBoard().get(0).get(0), 1);
+        Assertions.assertEquals(board.getBoard().get(1).get(0), 0);
+    }
+
+    @Test
+    void testMoveLeft() {
+        List<List<Integer>> temp = new ArrayList<>();
+
+        temp.add(new ArrayList<>());
+        temp.add(new ArrayList<>());
+
+        temp.get(0).add(1); temp.get(0).add(0);
+        temp.get(1).add(2); temp.get(1).add(3);
+
+        Board board = new Board(temp);
+        board.moveLeft();
+
+        Assertions.assertEquals(board.getBoard().get(0).get(0), 0);
+        Assertions.assertEquals(board.getBoard().get(0).get(1), 1);
+    }
+
+    @Test
+    void testMoveRight() {
+        List<List<Integer>> temp = new ArrayList<>();
+
+        temp.add(new ArrayList<>());
+        temp.add(new ArrayList<>());
+
+        temp.get(0).add(0); temp.get(0).add(1);
+        temp.get(1).add(2); temp.get(1).add(3);
+
+        Board board = new Board(temp);
+        board.moveRight();
+
+        Assertions.assertEquals(board.getBoard().get(0).get(0), 1);
+        Assertions.assertEquals(board.getBoard().get(0).get(1), 0);
+    }
+
+}
